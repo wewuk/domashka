dda532498687358f58e85734d6dd9c1dbce707c2
Даниил <wewuk@mail.ru>
2025-07-07 00:12:42 +0300

    Add files via upload

--- Вёрстка_макета_№3/js/main.js
+++ Вёрстка_макета_№3/js/main.js
@@ -1,7 +1,12 @@
 const burger = document.querySelector('.burger-icon')
 const body = document.querySelector('.body')
 const yakori = document.querySelectorAll('.nav__link')
+const gift = document.querySelector('.about__img-link')
+const modal = document.querySelector('.modal')
+const modalCloseBtn = document.querySelector('.modal__cancel')
 
+// ===================================================================
+// ===================================================================
 const openBurger = () => {
     body.classList.toggle('body--open-menu')
 }
@@ -20,3 +25,31 @@ document.addEventListener('keydown', event => {
         closeBurger()
     }
 })
+// ===================================================================
+// ===================================================================
+const closeModal = () => {
+    body.classList.remove('body-modal--open')
+}
+
+const openModal = () => {
+    body.classList.add('body-modal--open')
+}
+
+gift.addEventListener('click', openModal)
+
+modalCloseBtn.addEventListener('click', closeModal)
+
+modal.addEventListener('click', event => {
+    const target = event.target
+    if (target && target.classList.contains('modal')) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape' && body.classList.contains('body-modal--open')) {
+        closeModal()
+    }
+})
+// ===================================================================
+// ===================================================================
