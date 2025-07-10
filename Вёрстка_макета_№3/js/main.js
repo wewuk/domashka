dce7e37045cf349f02262ac7eb5e1f19d3377c64
Даниил <wewuk@mail.ru>
2025-07-11 00:11:55 +0300

    Add files via upload

--- Вёрстка_макета_№3/js/main.js
+++ Вёрстка_макета_№3/js/main.js
@@ -6,7 +6,7 @@ const modal = document.querySelector('.modal')
 const modalCloseBtn = document.querySelector('.modal__cancel')
 
 // ===================================================================
-// ===================================================================
+// ========================burger===========================================
 const openBurger = () => {
     body.classList.toggle('body--open-menu')
 }
@@ -26,7 +26,7 @@ document.addEventListener('keydown', event => {
     }
 })
 // ===================================================================
-// ===================================================================
+// ===========================modal========================================
 const closeModal = () => {
     body.classList.remove('body-modal--open')
 }
@@ -50,6 +50,57 @@ document.addEventListener('keydown', event => {
     if (event.code === 'Escape' && body.classList.contains('body-modal--open')) {
         closeModal()
     }
+})
+// ===================================================================
+// ===========================tab========================================
+const tabControls = document.querySelector('.tab-controls')
+
+tabControls.addEventListener('click', event => {
+
+    const tabControl = event.target.closest('.tab-controls__link')
+
+    if (!tabControl) return
+    event.preventDefault()
+    if (tabControl.classList.contains('tab-controls__link--active')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+
+    document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+    document.querySelector(tabContentID).classList.add('tab-content--show')
+
+    document.querySelector('.tab-controls__link--active').classList.remove('tab-controls__link--active')
+    tabControl.classList.add('tab-controls__link--active')
+})
+// ===================================================================
+// =========================accordion==========================================
+const accordionLists = document.querySelectorAll('.accordion-list')
+
+accordionLists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+        const accordionList = e.currentTarget
+        const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+        const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+        const accordionControl = e.target.closest('.accordion-list__control')
+        if (!accordionControl) return
+        const accordionItem = accordionControl.parentElement
+        const accordionContent = accordionControl.nextElementSibling
+
+        if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+            accordionOpenedItem.classList.remove('accordion-list__item--opened')
+            accordionOpenedContent.style.maxHeight = null
+        }
+        accordionItem.classList.toggle('accordion-list__item--opened')
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+        } else {
+            accordionContent.style.maxHeight = null
+        }
+
+    })
 })
 // ===================================================================
 // ===================================================================
